617a1c38c03d8d95ddd1da1da2fb9582aec3f0b5
Buns Baby <bunsbabyit@gmail.com>
2023-01-09 06:34:58 +0700

    Handle Led and Card Adafruit

--- frontend/screens/room_detail.screen.js
+++ frontend/screens/room_detail.screen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, SafeAreaView, FlatList, Pressable, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, SafeAreaView, FlatList, Pressable, TouchableOpacity, ScrollView } from "react-native";
 import React, { useState, useEffect } from "react";
 import IconM from "react-native-vector-icons/MaterialIcons";
 import Button from "../component/button.component";
@@ -8,6 +8,21 @@ import AnalyticsItem from "../component/analytics.component";
 
 const name = "Room 101";
 
+const PERSONLIST = [
+  {
+    id: "9486880",
+    name: "Nguyễn Tuấn Minh",
+  },
+  {
+    id: "261046",
+    name: "Đặng Tiến Mạnh",
+  },
+  {
+    id: "12634322",
+    name: "Phạm Ngọc Quang",
+  },
+]
+
 const RoomDetail = ({route, navigation}) => {
 
   const [status, setStatus] = useState(0)
@@ -15,23 +30,75 @@ const RoomDetail = ({route, navigation}) => {
   const [end, setEnd] = useState(new Date())
 
   const Book = () => {
-    navigation.navigate("BOOKING", { setStatus, setStart, setEnd })
+    navigation.navigate("BOOKING", { roomStatus: status, setStatus, setStart, setEnd })
   }
 
   const [doorStatus, setDoorStatus] = useState("0");
   const [doorTimestamp, setDoorTimestamp] = useState("0");
+
+  const [personList, setPersonList] = useState([]);
+  const [noPeople, setNoPeople] = useState(0);
+  const [personData, setPersonData] = useState()
+
+  const [lastUpdate, setLastUpdate] = useState(0);
+  const [timeFlag, setTimeFlag] = useState(new Date());
+
+  const removePeople = (e) => {
+    var array = [...personList]; // make a separate copy of the array
+    var array1 = array.filter(item => item.id == e.id)[0]
+    var index = array.indexOf(array1)
+    if (index !== -1) {
+      array.splice(index, 1);
+      setPersonList(array);
+    }
+  }
+
+  useEffect(() => {
+    if(lastUpdate.valueOf() > timeFlag.valueOf()){
+      if(personList.some(item => personData.id == item.id)){
+        removePeople(personData)
+        setNoPeople(noPeople - 1)
+      }
+      else {
+        personData && setPersonList(list => [...list, personData])
+        setNoPeople(noPeople + 1)
+      }
+      
+      setTimeFlag(new Date())
+    }
+  }, [lastUpdate])
+
+  useEffect(() => {
+
+  }, [timeFlag])
+
+  const postPersonList = async () => {
+    const result = await axios.get(
+      `https://io.adafruit.com/api/v2/HungNguyenHung/feeds/bbc-card`
+    );
+    const dataId = result.data.last_value
+    const dataName = PERSONLIST.filter(e => e.id == dataId)[0].name
+    const data = {
+      id: dataId,
+      name: dataName,
+    }
+    setPersonData(data)
+    setLastUpdate(new Date(result.data.updated_at))
+    }
+  
+
   const getValue = async () => {
     const result = await axios.get(
     `https://io.adafruit.com/api/v2/HungNguyenHung/feeds/bbc-magnetic`
     );
     setDoorStatus(result.data.last_value);
     setDoorTimestamp(result.data.updated_at);
-    // getStatus();
   }
   useEffect(() => {
     const isMounted = true;
     const intervalid = setInterval(() => {
         getValue();
+        postPersonList();
     }, 1000);
     return () => {
         clearInterval(intervalid);
@@ -74,7 +141,7 @@ const RoomDetail = ({route, navigation}) => {
               ROOM DETAIL
             </Text>
           </View>
-          <SafeAreaView>
+          <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
             <View
               style={{
                 paddingHorizontal: 30,
@@ -125,7 +192,17 @@ const RoomDetail = ({route, navigation}) => {
                 title="Book"
               />
             </View>
-          </SafeAreaView>
+            <View style={style.personTitleContainer}>
+              <Text style={style.personTitle}>Số lượng người trong phòng là: {noPeople}</Text>
+            </View>
+            <View style={style.personContainer}>
+              {personList.map(person => (
+                <View style={style.personBox}>
+                  <Text style={style.personName}>{person.name}</Text>
+                </View>
+              ))}
+            </View>
+          </ScrollView>
         </View>
     );
 };
@@ -172,6 +249,36 @@ const style = StyleSheet.create({
         flexDirection: "column",
         marginBottom: 0,
     },
+    personContainer: {
+      justifyContent: 'center',
+      marginLeft: 24,
+      marginRight: 24,
+    },
+    personBox: {
+      backgroundColor: "#fff",
+      marginBottom: 8,
+      borderRadius: 8,
+    },
+    personName: {
+      fontSize: 18,
+      fontWeight: "600",
+      letterSpacing: 1,
+      color: "#444",
+      marginTop: 8,
+      marginBottom: 8,
+      marginLeft: 10,
+    },
+    personTitle: {
+      fontSize: 20,
+      fontWeight: "700",
+      letterSpacing: 1,
+      color: "#fff",
+    },
+    personTitleContainer: {
+      marginTop: 0,
+      marginBottom: 12,
+      marginLeft: 24,
+    }
 });
 
 export default RoomDetail;
